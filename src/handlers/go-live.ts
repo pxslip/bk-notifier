328f7d94d4c049d868aa01219eb2a8322206ce28
Will K (pxslip) <dev@pxslip.com>
2022-04-05 05:03:24 +0000

    Updated naming

--- src/handlers/go-live.ts
+++ src/handlers/go-live.ts
@@ -9,23 +9,23 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  *
  */
 export default async function (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-    let response: APIGatewayProxyResult;
-    try {
-        response = {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
-        };
-    } catch (err) {
-        console.log(err);
-        response = {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
-    }
+  let response: APIGatewayProxyResult;
+  try {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'hello world',
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'some error happened',
+      }),
+    };
+  }
 
-    return response;
+  return response;
 }
